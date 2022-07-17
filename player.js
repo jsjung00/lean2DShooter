93a9ab33854c7b55ea15a2da48ce4404c788c800
jsjung00 <justinsoljung@gmail.com>
2022-07-16 22:23:11 -0700

    add player shooting

--- player.js
+++ player.js
@@ -2,11 +2,12 @@ class Player {
   constructor(x, y) {
     this.health = 100;
     this.pos = createVector(x, y);
-    this.width = 50;
-    this.height = 50;
+    this.rad = 50;
     this.speed = 3;
+    this.gunOutBy = 10;
   }
   show() {
+    push();
     var mouse = createVector(mouseX, mouseY);
     var dir = p5.Vector.sub(mouse, this.pos);
     dir.normalize();
@@ -16,11 +17,12 @@ class Player {
     rotate(angleBetween);
     //draw the player
     fill(0);
-    ellipse(0, 0, this.width, this.height);
+    ellipse(0, 0, this.rad, this.rad);
     //draw the gun
-    translate(0, 10);
+    translate(0, this.gunOutBy);
     fill(255);
     rect(0, 0, 10, 50);
+    pop();
   }
   //takes in string "N", "E", "S", "W" to determine direction to move
   move(dir) {
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var player;
+let bullets = [];
 function setup() {
   createCanvas(1000, 1000);
   player = new Player(width / 2, height / 2);
@@ -7,8 +8,33 @@ function setup() {
 
 function draw() {
   background(220);
+  //Player
   moving();
   player.show();
+  //Bullets
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    //remove if bullet is off screen
+    const x_pos = bullets[i].pos.x;
+    const y_pos = bullets[i].pos.y;
+    if (x_pos < 0 || x_pos > width || y_pos < 0 || y_pos > height) {
+      bullets.splice(i, 1);
+    } else {
+      bullets[i].show();
+      bullets[i].update();
+    }
+  }
+}
+
+function mouseClicked() {
+  var mouse = createVector(mouseX, mouseY);
+  const pos = this.player.pos;
+  var dir = p5.Vector.sub(mouse, pos);
+  dir.normalize();
+  let bulletPos = p5.Vector.add(
+    pos,
+    p5.Vector.mult(dir, this.player.rad + this.player.gunOutBy)
+  );
+  bullets.push(new Bullet(bulletPos.x, bulletPos.y, dir));
 }
 
 function moving() {
